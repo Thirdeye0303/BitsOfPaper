42dc814feac7fae41ab36fc0e4d1fee8bba52e07
Thirdeye0303 <73580854+Thirdeye0303@users.noreply.github.com>
2021-07-17 21:14:17 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,20 @@
+class Paper{
+constructor(x,y,r){
+var options = {
+    isStatic: false,
+    restitution: 0.3,
+    friction: 0,
+    density:1.2
+}
+this.r = r
+this.crumpledImage = loadImage("paper.png");
+this.body = Bodies.circle(x,y,r,options)
+World.add(world, this.body)
+}
+display(){
+push()
+translate(this.body.position.x,this.body.position.y)
+image(this.crumpledImage,0,0, this.r, this.r)
+pop()
+}
+}
